302409385624775f49770e502937b2f57b745b85
Araz Nadzhafov <brainless11.5.1994@gmail.com>
2020-11-03 12:49:37 +0300

    About

---
+++ ATG/Calculator/About/Cell/AboutCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AboutTableViewCell.swift
+//  ATG
+//
+//  Created by Наджафов Араз on 03.11.2020.
+//
+
+import UIKit
+
+class AboutCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
---
+++ ATG/Calculator/About/Model/AboutModel.swift
@@ -0,0 +1,14 @@
+//
+//  AboutModel.swift
+//  ATG
+//
+//  Created by Наджафов Араз on 03.11.2020.
+//
+
+import Foundation
+
+struct AboutModel {
+    
+    var image: String
+    
+}
